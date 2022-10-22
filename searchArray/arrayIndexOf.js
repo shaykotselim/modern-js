351cc2fd478f260afe2c95df91f978ce9e8ba118
shaykot43500 <hossainshaykot@gmail.com>
2022-10-22 21:35:29 +0600

    added new

---
+++ searchArray/arrayIndexOf.js
@@ -0,0 +1,18 @@
+/**
+    array.indexOf() method
+
+    The indexOf() method returns the first index at which again element can be 
+    found in an array. It return -1 if the elements does not exist in the array.
+
+    Ex :- 
+     Let's got back to our example. Let's find the index of 3 in the array.
+    
+ */
+        // Ex :- 01
+
+        const numberArray = [10, 11, 3, 20, 33, 4,];
+        const indexOfThree = numberArray.indexOf(3);
+        console.log(indexOfThree); // 2 
+
+        //Note: indexOfArray return first element -1;
+        
