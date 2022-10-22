22413489b612e00fb1c17b44d2269eb6652feb7c
shaykot43500 <hossainshaykot@gmail.com>
2022-10-22 21:14:41 +0600

    added some new

---
+++ searchArray/arrayIncludes.js
@@ -0,0 +1,14 @@
+/**
+    array.include() method 
+    
+    The includes() method determine whether an array.includes() a certain value 
+    and returns true or false as approprite.
+
+    Ex :- So, in the example above the, if we want to check if 20 is one of the 
+    elements in the array, we can do this....
+
+ */
+            //Ex :- 01
+        const numbersArray = [ 10, 12 , 34 , 43, 22, 2];
+        const includesFortyThree = numbersArray.includes(43);
+        console.log(includesFortyThree) //Ture;
