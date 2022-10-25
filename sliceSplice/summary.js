05b0933d88308ecc80913d23a1b32c8fdf5658f1
shaykot43500 <hossainshaykot@gmail.com>
2022-10-26 01:15:04 +0600

    added summary.js

---
+++ sliceSplice/summary.js
@@ -0,0 +1,15 @@
+/**
+ * Conclusion-:
+ * 
+    slice() method
+ 
+    *The slice() method can be used to create a copy of an array or return a portion of an array. It is important to note that the slice() method does not alter the original array but instead creates a shallow copy.
+  
+  
+    splice() method
+  
+    *The splice() method is used to add or remove elements of an existing array and the return value will be the removed items from the array.
+ * 
+ */
+
+// For more-: https://www.freecodecamp.org/news/javascript-slice-and-splice-how-to-use-the-slice-and-splice-js-array-methods/
