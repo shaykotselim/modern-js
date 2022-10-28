21a574a3a1e810eeffd1330a6f69512dd55d9f4b
shaykot43500 <hossainshaykot@gmail.com>
2022-10-28 23:44:22 +0600

    added unshift.js

---
+++ pushUnshiftConcat/unshift.js
@@ -0,0 +1,13 @@
+/**
+ * unshift() method
+ 
+    The unshift() method is used to add an element to the beginning of an array;
+ 
+ */
+    // Ex-: 1;
+
+    const unshiftArr = ['First-Item','Second-Item', 'Third-Item'];
+    unshiftArr.unshift('Fourth-Item','Five-Item');
+        
+    console.log(unshiftArr);//['Fourth-Item','Five-Item','First-Item','Second-Item','Third-Item'];
+    
