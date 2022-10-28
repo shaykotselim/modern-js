cc7e59fc7c82f41f258ccb94e6cfe3bad3c613ed
shaykot43500 <hossainshaykot@gmail.com>
2022-10-28 23:25:34 +0600

    added push.js

---
+++ pushUnshiftConcat/push.js
@@ -0,0 +1,17 @@
+/**
+    * push() method
+  
+    The push() method is used for adding an element to the end of array;
+  
+    Note -: It would make sense to add newer items to the end of array, so that 
+            we could finish our earlier task finish;
+
+ */
+
+    // Ex - : 1;
+
+        const arr = ['First-item', 'Second-item', 'Third-item',]
+        arr.push('Fourth-item');
+
+    console.log(arr.length)// 4
+    console.log(arr)//['First-item', 'Second-item', 'Third-item', 'Fourth-item']
