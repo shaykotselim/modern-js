8a48bccc2a1c2e9bd204e6923c5fa986b285b16d
shaykot43500 <hossainshaykot@gmail.com>
2022-10-30 21:17:50 +0600

    change little

--- mapReduce/summary.js
+++ mapReduce/summary.js
@@ -1,10 +1,10 @@
 /**
      For more know about map array:
-    Here -:    https://www.freecodecamp.org/news/            javascript-map-how-to-use-the-js-map-function-array-method/
+    Here -:https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/
  */
 
 /**
     For more know about reduce array:
-    Here -:     https://www.freecodecamp.org/news/the-ultimate-guide-to-javascript-array-methods-reduce/
+    Here -:https://www.freecodecamp.org/news/the-ultimate-guide-to-javascript-array-methods-reduce/
 
  */
