3b87f126a9bd642a1d95c91931a6ded105ab89f7
shaykot43500 <hossainshaykot@gmail.com>
2022-11-02 01:23:29 +0600

    added keys.js

---
+++ keysValuesEntries/keys.js
@@ -0,0 +1,14 @@
+const myobj = { 
+        product: 'Laptop', 
+        brand : 'Dell',
+        price : 5500
+}
+
+const keysobj = Object.keys(myobj);
+// console.log(keysobj);// [ 'product', 'brand', 'price' ]
+
+const valuesobj = Object.values(myobj);
+// console.log(valuesobj);//[ 'Laptop', 'Dell', 5500 ]
+
+const entriesobj = Object.entries(myobj);
+console.log(entriesobj);// 
