04d8e46e30cd1104c2b5ab5c9eb274302f87637d
shaykot43500 <hossainshaykot@gmail.com>
2022-11-03 00:20:55 +0600

    added summary.js

--- defaultParameters/defaultPrameter.js
+++ defaultParameters/defaultPrameter.js
@@ -7,3 +7,12 @@
                 console.log(x+y); // NaN
             }  
             defaultAdd()
+            // I  would get NaN, not a number;
+
+            //Ex : 2
+
+            function defaultAdd1(x = 6 , y = 7){
+                console.log(x + y)// 13
+            };
+            defaultAdd1()
+            // You get 12! This means if you don’t specifically add values to this function when you call it, it will use the default values.
---
+++ defaultParameters/summary.js
@@ -0,0 +1,3 @@
+/**
+    For more:  https://www.freecodecamp.org/news/learn-es6-the-dope-way-part-iv-default-parameters-destructuring-assignment-a-new-es6-method-44393190b8c9/
+ */
