c5ca540ffa892fdb661c0770c10fadb7cecc8c27
shaykot43500 <hossainshaykot@gmail.com>
2022-11-03 00:14:48 +0600

    added defaultPrameter

---
+++ defaultParameters/defaultPrameter.js
@@ -0,0 +1,9 @@
+/**
+        Default function parameters allow you to initialize default values if either no values are passed, or if undefined is passed. Before, if you had something like this:
+ */
+
+            //Ex : 01
+            function defaultAdd (x, y){
+                console.log(x+y); // NaN
+            }  
+            defaultAdd()
